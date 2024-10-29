0c75e417c762fb1e0a47de25ab0f54d51ae24586
yuran-nhassengo <yurannhassengo@gmail.com>
2024-10-29 03:04:21 +0200

    add css

--- index.php
+++ index.php
@@ -3,15 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Demo</title>
+
+    <style>
+        body{
+            display: grid;
+            place-items:center;
+            height:100vh;
+            margin:0;
+            font-family:sans-serif;
+        }
+    </style>
 </head>
 <body>
     <h1>
 
-        <?php
-            echo "Hello, World";
-        ?>
-
+       You have read "Dark Matter."
     </h1>
 </body>
 </html>
